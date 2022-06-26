ea4badca541d8e60d002a864deb04d0406f8af2c
Gabriel Donadel Dall'Agnol <donadeldev@gmail.com>
2022-06-26 20:38:31 -0300

    fix: Login redirect

--- src/views/Login/index.tsx
+++ src/views/Login/index.tsx
@@ -11,7 +11,6 @@ import { useNavigate } from "react-router-dom";
 
 const uiConfig = {
   signInFlow: "popup",
-  signInSuccessUrl: "/contracts",
   signInOptions: [
     GoogleAuthProvider.PROVIDER_ID,
     GithubAuthProvider.PROVIDER_ID,
